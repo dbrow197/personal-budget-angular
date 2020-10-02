f9a14d051eabb10a9f2a6a76f064139d38b0906e
Dan Brown <dbrow197@uncc.edu>
2020-10-01 23:45:32 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -2,33 +2,31 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+
+
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat out',
-        budget: 25
-    },
-    {
-        title:'Rent',
-        budget: 375
-    },
-    {
-        title: 'Groceries',
-        budget: 110
-    },
-    ]
-};
+
+//var fs = require('fs');
+//var obj = JSON.parse(fs.readFileSync('/newBudget', 'utf8'));
 
 app.get('/hello', (req, res) =>{
    res.send('Hello World!');
 });
 
+const data = require('./newBudget.json');
+
+app.get('/budget', function (req, res) {
+  res.header("Content-Type",'application/json');
+  res.send(JSON.stringify(data));
+})
+
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    res.json(myBudget);
 });
+
 
 app.listen(port, () => {
     console.log(`Example app listening at https://localhost:${port}`);
 });
+
