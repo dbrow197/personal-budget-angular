243e634b7b5b9abff19c72a0a82b1a8b327813ce
Dan Brown <dbrow197@uncc.edu>
2020-10-12 10:33:44 -0400

    fixed cors

--- server.js
+++ server.js
@@ -1,59 +1,20 @@
 const express = require('express');
-//const cors = require('cors');
+const cors = require('cors');
 const app = express();
 const port = 3000;
-
-//app.use(cors());
-app.use('/', express.static('public'));
-
-const budget = {
-
-    myBudget: [
-   {
-        title: "Eat out",
-        budget: 100
-    },
-    {
-        title: "Rent",
-        budget: 1200
-    },
-    {
-        title: "Groceries",
-        budget: 250
-    },
-    {
-        title: "Phone",
-        budget: 89
-    },
-    {
-        title: "Car",
-        budget: 400
-    },
-    {
-        title: "Gym",
-        budget: 35
-    },
-    {
-        title: "DigitalOcean",
-        budget: 4
-    },
-    ]
-};
-
-//app.get('/hello', (req, res) =>{
-//   res.send('Hello World!');
-//});
-
 const data = require('./newBudget.json');
+
+app.use(cors());
+app.use('/', express.static('public'));
 
 app.get('/budget', function (req, res) {
   res.header("Content-Type",'application/json');
   res.send(JSON.stringify(data));
 })
 
-app.get('/budget', (req, res) => {
-    res.json(budget);
-});
+//app.get('/budget', (req, res) => {
+//    res.json(budget);
+//});
 //possible myBudget
 
 app.listen(port, () => {
